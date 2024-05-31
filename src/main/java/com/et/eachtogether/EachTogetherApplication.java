2217516f46bd7bffff0d05ea32d8f92d108ed0b7
DESKTOP-UF6GC6B\khu09 <khu4237@gmail.com>
2024-05-31 16:31:58 +0900

    init commit

---
+++ src/main/java/com/et/eachtogether/EachTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.et.eachtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EachTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EachTogetherApplication.class, args);
+    }
+
+}
