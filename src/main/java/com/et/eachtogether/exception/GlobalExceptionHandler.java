163f8f1d4a623d9378d30fb24be950943dce6db0
DESKTOP-UF6GC6B\khu09 <khu4237@gmail.com>
2024-06-03 19:25:23 +0900

    글로벌 예외 핸들러 추가

---
+++ src/main/java/com/et/eachtogether/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.et.eachtogether.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleException(IllegalArgumentException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
