6f7050587822a1d06f066d3aabcfa839f31fbc78
DESKTOP-UF6GC6B\khu09 <khu4237@gmail.com>
2024-06-03 14:56:33 +0900

    JwtAuthenticationFilter 코드 정리

--- src/main/java/com/et/eachtogether/security/JwtAuthenticationFilter.java
+++ src/main/java/com/et/eachtogether/security/JwtAuthenticationFilter.java
@@ -8,13 +8,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collection;
 
 @Slf4j
 @RequiredArgsConstructor
